7a1d8078fabb96dda1255e8a22869ed10c9a6220
Dominik Ryczek <dominik.ryczek@expereo.com>
2024-03-21 12:57:12 +0100

    Solution for helloapi task

---
+++ tasks/helloapi.js
@@ -0,0 +1,29 @@
+const fetchToken = require('../utils').fetchToken;
+const getTaskData = require('../utils').getTaskData;
+const postAnswer = require('../utils').postAnswer;
+const fetchInfo = require('../utils').fetchInfo;
+
+const info = fetchInfo();
+
+const taskName = 'helloapi';
+
+const apikey = info.apikey;
+const url = info.url;
+
+let accessToken;
+
+
+fetchToken(url, taskName, apikey)
+    .then(token => {
+        if(token) {
+            accessToken = token;
+            return getTaskData(url, accessToken);
+        }
+    })
+    .then((data) => {
+        const cookie = data.cookie;
+        postAnswer(url, accessToken, cookie);
+    })
+    .catch(error => {
+        console.log(`Error: ${error}`);
+    });
