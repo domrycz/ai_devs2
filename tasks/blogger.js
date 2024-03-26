99d5702b6038db386712c57d25a923e3f8c98ba1
Dominik Ryczek <dominik.ryczek@expereo.com>
2024-03-26 16:33:06 +0100

    Blogger task solution

---
+++ tasks/blogger.js
@@ -0,0 +1,52 @@
+import { fetchInfo, fetchToken, getTaskData, postAnswer } from '../utils/utils.js';
+import OpenAI from "openai";
+
+const info = fetchInfo();
+
+const taskName = 'blogger';
+const apikey = info.apikey;
+const url = info.url;
+const openAiKey = info.openAiKey;
+
+let accessToken;
+
+fetchToken(url, taskName, apikey)
+    .then(token => {
+        if (token) {
+            accessToken = token;
+            return getTaskData(url, accessToken);
+        }
+    })
+    .then(taskData => {
+        const chaptersTitles = taskData.blog;
+        return writeContent(chaptersTitles, openAiKey);
+    })
+    .then(arrayWithChapters => {
+        const answer = {answer: arrayWithChapters};
+        postAnswer(url, accessToken, arrayWithChapters);
+    })
+    .catch(error => {
+        console.error(`Error: ${error}`);
+    })
+
+const writeContent = async (chaptersTitles, openAiKey) => {
+    const systemInstructions = 'You are a polish food blogger specialized in Italian cuisine. You respond to given instructions only with blog posts in polish language without any additional content.';
+    const userInstructions = `Create blog post which contains these chapters: ${chaptersTitles.join(', ')}. At the end of content of every chapter put three hash (#) signs. Do not do it at the end of the last chapter.`;
+
+    const openai = new OpenAI({apiKey: openAiKey});
+    let chatResponse;
+    try {
+        chatResponse = await openai.chat.completions.create({
+            messages: [
+                {role: "system", content: systemInstructions},
+                {role: "user", content: userInstructions}
+            ],
+            model: "gpt-3.5-turbo",
+        });
+    } catch (error) {
+        throw new Error(`Error calling Chat GPT: ${error}`);
+    }
+    const chatMessage = chatResponse.choices[0].message.content;
+
+    return chatMessage.replace(/###$/, '').split('###');
+}
