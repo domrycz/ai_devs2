37615c14e772c9cd7acc932cc6d05784e3417a47
Dominik Ryczek <dominik.ryczek@expereo.com>
2024-03-30 14:26:37 +0100

    Task inprompt finished

---
+++ tasks/inprompt.js
@@ -0,0 +1,82 @@
+import { fetchInfo, fetchToken, getTaskData, postAnswer } from '../utils/utils.js';
+import OpenAI from "openai";
+
+const info = fetchInfo();
+
+const taskName = 'inprompt';
+const apikey = info.apikey;
+const url = info.url;
+
+const openai = new OpenAI({apiKey: info.openAiKey});
+
+let accessToken;
+let inputFromTask;
+let question;
+
+fetchToken(url, taskName, apikey)
+    .then(token => {
+        if (token) {
+            accessToken = token;
+            return getTaskData(url, accessToken);
+        }
+    })
+    .then(taskData => {
+        inputFromTask = taskData.input;
+        question = taskData.question;
+        return getName(question);
+    })
+    .then(name => {
+        const context = inputFromTask.filter(sentence => sentence.includes(name));
+        return getAnswerFromModel(context, question);
+    })
+    .then(modelAnswer => {
+        postAnswer(url, accessToken, modelAnswer);
+    })
+    .catch(error => {
+        console.error(`# Error: ${error}`);
+    })
+
+const getName = async (question) => {
+    const systemInstructions = 'Return only and exclusively one word, without any comments and this word should be valid name from the given polish sentence.';
+    const userInstructions = question;
+    let name;
+    try {
+        const chatResponse = await openai.chat.completions.create({
+            messages: [
+                {role: "system", content: systemInstructions},
+                {role: "user", content: userInstructions}
+            ],
+            model: "gpt-3.5-turbo",
+        });
+        name = chatResponse.choices[0].message.content.replace('.', '');
+        if(!containsOnlyLetters(name)) {
+            throw new Error(`Name is not valid: ${name}`);
+        }
+    } catch (error) {
+        throw new Error(`Error calling LLM to get name: ${error}`);
+    }
+    return name;
+}
+
+const getAnswerFromModel = async (context, question) => {
+    const systemInstructions = `Answer questions based on the given context. Do not add any additional information.
+    context: ### \n${context.join('\n')}\n###`;
+    let modelAnswer;
+    try {
+        const chatResponse = await openai.chat.completions.create({
+            messages: [
+                {role: "system", content: systemInstructions},
+                {role: "user", content: question}
+            ],
+            model: "gpt-3.5-turbo",
+        });
+        modelAnswer = chatResponse.choices[0].message.content;
+    } catch (error) {
+        throw new Error(`Error calling LLM to get an answer: ${error}`);
+    }
+    return modelAnswer;
+}
+
+const containsOnlyLetters = (inputString) => {
+    return /^[a-zA-Z]*$/.test(inputString);
+}
